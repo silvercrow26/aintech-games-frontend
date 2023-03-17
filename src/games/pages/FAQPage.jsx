ee08d4113f5266fe1ed0735459a7bc9b6b293b27
Silvercrow <maatiasf97@gmail.com>
2023-03-17 20:33:09 -0300

    FAQ updated

--- src/games/pages/FAQPage.jsx
+++ src/games/pages/FAQPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { faTowerCell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './FAQPage.scss';
+import '../SASS/FAQPage.scss';
 export const FAQPage = () => {
     return (
         <>
