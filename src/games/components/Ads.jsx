67d8a4cf7d1602ebfdea99817cc011232a8a799f
Silvercrow <maatiasf97@gmail.com>
2023-05-08 14:44:29 -0400

    googleads implemented

--- src/games/components/Ads.jsx
+++ src/games/components/Ads.jsx
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react'
 
 export const Ads = () => {
 
-    // const componentDidMount = () => {
-    //     (window.adsbygoogle = window.adsbygoogle || []).push({});
-    // }
+    const componentDidMount = () => {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
 
-    // useEffect(() => {
-    //     componentDidMount();
-    // }, [])
+    useEffect(() => {
+        componentDidMount();
+    }, [])
 
     return (
         <>
             <ins className="adsbygoogle"
                 style={{ display: 'block' }}
+                data-ad-format="fluid"
+                data-ad-layout-key="-c3-g+h+bq-mp"
                 data-ad-client="ca-pub-4288271666568125"
-                data-ad-slot="8298384110"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
+                data-ad-slot="2204313702"></ins>
         </>
     )
 }
